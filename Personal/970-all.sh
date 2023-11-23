ab903c8071a58618196e556a2764d6c9646c9930
Yuri Kuit <yuri.kuit+github@gmail.com>
2023-11-23 13:53:22 +0100

    cleanup 970

--- Personal/970-all.sh
+++ Personal/970-all.sh
@@ -2,19 +2,6 @@
 #set -e
 ##################################################################################################################
 # Author    : Erik Dubois
-# Website   : https://www.erikdubois.be
-# Website   : https://www.alci.online
-# Website   : https://www.ariser.eu
-# Website   : https://www.arcolinux.info
-# Website   : https://www.arcolinux.com
-# Website   : https://www.arcolinuxd.com
-# Website   : https://www.arcolinuxb.com
-# Website   : https://www.arcolinuxiso.com
-# Website   : https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
 ##################################################################################################################
 #tput setaf 0 = black
 #tput setaf 1 = red
@@ -61,30 +48,13 @@ sudo systemctl enable fstrim.timer
 echo
 
 echo
-echo "Testing if qemu agent is still active"
-result=$(systemd-detect-virt)
-echo "Systemd-detect-virt = "
-test=$(systemctl is-enabled qemu-guest-agent.service)
-echo "Is qemu guest agent active = "
-echo "If one of the parameters is empty you get unary parameter"
-echo "Nothing is wrong however"
-if [ $test == "enabled" ] && [ $result == "none" ] || [ $result == "oracle" ]; then
-	echo
-	echo "Disable qemu agent service"
-	sudo systemctl disable qemu-guest-agent.service
-	echo
-fi
-
-echo
 echo "Adding nanorc settings"
-
 if [ -f /etc/nanorc ]; then
 	sudo cp $installed_dir/settings/nano/nanorc /etc/nanorc
 fi
 
 echo
 echo "Adding ubuntu keyserver"
-
 if ! grep -q "hkp://keyserver.ubuntu.com:80" /etc/pacman.d/gnupg/gpg.conf; then
 echo '
 keyserver hkp://keyserver.ubuntu.com:80' | sudo tee --append /etc/pacman.d/gnupg/gpg.conf
