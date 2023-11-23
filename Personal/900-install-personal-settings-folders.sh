49362c21035a0e420a53d4ed712406cc99caeb65
Yuri Kuit <yuri.kuit+github@gmail.com>
2023-11-23 13:01:00 +0100

    cleanup 900 + belonging files/folders

--- Personal/900-install-personal-settings-folders.sh
+++ Personal/900-install-personal-settings-folders.sh
@@ -48,7 +48,6 @@ echo
 [ -d $HOME"/.config/gtk-3.0" ] || mkdir -p $HOME"/.config/gtk-3.0"
 [ -d $HOME"/.config/gtk-4.0" ] || mkdir -p $HOME"/.config/gtk-4.0"
 [ -d $HOME"/.config/variety" ] || mkdir -p $HOME"/.config/variety"
-[ -d $HOME"/.config/fish" ] || mkdir -p $HOME"/.config/fish"
 [ -d $HOME"/.config/neofetch" ] || mkdir -p $HOME"/.config/neofetch"
 [ -d $HOME"/DATA" ] || mkdir -p $HOME"/DATA"
 [ -d $HOME"/Insync" ] || mkdir -p $HOME"/Insync"
@@ -74,74 +73,69 @@ echo
 #uncommenting so that we see the bluetooth icon in our toolbars
 gsettings set org.blueberry use-symbolic-icons false
 
-if grep -q "ArcoLinux" /etc/os-release; then
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### Personal settings to install"
+echo "################################################################"
+tput sgr0
+echo
 
-	echo
-	tput setaf 2
-	echo "################################################################"
-	echo "################### Personal settings to install - ArcoLinux"
-	echo "################################################################"
-	tput sgr0
-	echo
+echo "Installing all shell files"
+echo
+cp $installed_dir/settings/shell-personal/.bashrc-personal ~/.bashrc-personal
+cp $installed_dir/settings/shell-personal/.zshrc-personal ~/.zshrc-personal
+echo
 
-	echo "Installing all shell files"
-	echo
-	cp $installed_dir/settings/shell-personal/.bashrc-personal ~/.bashrc-personal
-	cp $installed_dir/settings/shell-personal/.zshrc ~/.zshrc
-	sudo cp -f $installed_dir/settings/shell-personal/.zshrc /etc/skel/.zshrc
-	cp $installed_dir/settings/shell-personal/.zshrc-personal ~/.zshrc-personal
-	echo
+echo "Adding personal looks to /personal"
+echo
+sudo cp -arf $installed_dir/settings/personal-folder/personal-iso/* /personal
+echo
 
-	echo "Adding personal looks to /personal"
-	echo
-	sudo cp -arf $installed_dir/settings/personal-folder/personal-iso/* /personal
-	echo
-	
-	echo "To personal Kvantum setup"
-	echo
-	[ -d $HOME"/.config/Kvantum" ] || mkdir -p $HOME"/.config/Kvantum"
-	cp -r $installed_dir/settings/Kvantum/* $HOME/.config/Kvantum
-	[ -d /etc/skel/.config/Kvantum ] || sudo mkdir -p /etc/skel/.config/Kvantum
-	sudo cp -r $installed_dir/settings/Kvantum/* /etc/skel/.config/Kvantum
-	echo
+echo "To personal Kvantum setup"
+echo
+[ -d $HOME"/.config/Kvantum" ] || mkdir -p $HOME"/.config/Kvantum"
+cp -r $installed_dir/settings/Kvantum/* $HOME/.config/Kvantum
+[ -d /etc/skel/.config/Kvantum ] || sudo mkdir -p /etc/skel/.config/Kvantum
+sudo cp -r $installed_dir/settings/Kvantum/* /etc/skel/.config/Kvantum
+echo
 
-	echo
-	echo "To default xfce settings"
-	[ -d $HOME"/.config/xfce4/xfconf/xfce-perchannel-xml/" ] || mkdir -p $HOME"/.config/xfce4/xfconf/xfce-perchannel-xml/"
-	cp  $installed_dir/settings/xfce/xsettings.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
-	[ -d /etc/skel/.config/xfce4 ] || sudo mkdir -p /etc/skel/.config/xfce4
-	[ -d /etc/skel/.config/xfce4/xfconf ] || sudo mkdir -p /etc/skel/.config/xfce4/xfconf
-	[ -d /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml ] || sudo mkdir -p /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml
-	sudo cp  $installed_dir/settings/xfce/xsettings.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml
-	echo
+echo
+echo "To default xfce settings"
+[ -d $HOME"/.config/xfce4/xfconf/xfce-perchannel-xml/" ] || mkdir -p $HOME"/.config/xfce4/xfconf/xfce-perchannel-xml/"
+cp  $installed_dir/settings/xfce/xsettings.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
+[ -d /etc/skel/.config/xfce4 ] || sudo mkdir -p /etc/skel/.config/xfce4
+[ -d /etc/skel/.config/xfce4/xfconf ] || sudo mkdir -p /etc/skel/.config/xfce4/xfconf
+[ -d /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml ] || sudo mkdir -p /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml
+sudo cp $installed_dir/settings/xfce/xsettings.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml
+echo
 
-	echo 
-	echo "To default gtk-3.0 config"
-	[ -d $HOME"/.config/gtk-3.0" ] || mkdir -p $HOME"/.config/gtk-3.0"
-	cp  $installed_dir/settings/gtk3/settings.ini $HOME/.config/gtk-3.0
-	[ -d "/etc/skel/.config/gtk-3.0" ] || sudo mkdir -p "/etc/skel/.config/gtk-3.0"
-	sudo cp  $installed_dir/settings/gtk3/settings.ini /etc/skel/.config/gtk-3.0
-	echo
+echo 
+echo "To default gtk-3.0 config"
+[ -d $HOME"/.config/gtk-3.0" ] || mkdir -p $HOME"/.config/gtk-3.0"
+cp  $installed_dir/settings/gtk3/settings.ini $HOME/.config/gtk-3.0
+[ -d "/etc/skel/.config/gtk-3.0" ] || sudo mkdir -p "/etc/skel/.config/gtk-3.0"
+sudo cp $installed_dir/settings/gtk3/settings.ini /etc/skel/.config/gtk-3.0
+echo
 
-	echo
-	echo "Adding personal thunar to .config/thunar"
-	[ -d $HOME"/.config/Thunar" ] || mkdir -p $HOME"/.config/Thunar"
-	cp  $installed_dir/settings/thunar/uca.xml $HOME/.config/Thunar
-	[ -d /etc/skel/.config/Thunar ] || sudo mkdir -p /etc/skel/.config/Thunar
-	sudo cp  $installed_dir/settings/thunar/uca.xml /etc/skel/.config/Thunar
-	echo
+echo
+echo "Adding personal thunar to .config/thunar"
+[ -d $HOME"/.config/Thunar" ] || mkdir -p $HOME"/.config/Thunar"
+cp  $installed_dir/settings/thunar/uca.xml $HOME/.config/Thunar
+[ -d /etc/skel/.config/Thunar ] || sudo mkdir -p /etc/skel/.config/Thunar
+sudo cp $installed_dir/settings/thunar/uca.xml /etc/skel/.config/Thunar
+echo
 
-	echo
-	echo "Installing personal settings of variety"
-	[ -d $HOME"/.config/variety" ] || mkdir -p $HOME"/.config/variety"
-	[ -d $HOME"/.config/variety/Favorites" ] || mkdir -p $HOME"/.config/variety/Favorites"
-	cp $installed_dir/settings/variety/variety.conf ~/.config/variety/
-	cp $installed_dir/settings/variety/variety/Favorites/*.* ~/.config/variety/Favorites/
-	[ -d /etc/skel/.config/variety ] || sudo mkdir -p /etc/skel/.config/variety
-	sudo cp $installed_dir/settings/variety/variety.conf /etc/skel/.config/variety/
-	sudo cp $installed_dir/settings/variety/Favorites/*.* /etc/skel/.config/variety/Favorites/
-	echo
-fi
+echo
+echo "Installing personal settings of variety"
+[ -d $HOME"/.config/variety" ] || mkdir -p $HOME"/.config/variety"
+[ -d $HOME"/.config/variety/Favorites" ] || mkdir -p $HOME"/.config/variety/Favorites"
+cp $installed_dir/settings/variety/variety.conf ~/.config/variety/
+cp -r $installed_dir/settings/variety/variety/Favorites/* ~/.config/variety/Favorites/
+[ -d /etc/skel/.config/variety ] || sudo mkdir -p /etc/skel/.config/variety
+sudo cp $installed_dir/settings/variety/variety.conf /etc/skel/.config/variety/
+sudo cp -r $installed_dir/settings/variety/Favorites/* /etc/skel/.config/variety/Favorites/
+echo
 
 echo
 tput setaf 6
