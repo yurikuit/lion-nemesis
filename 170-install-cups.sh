1d84d7e873f3cf3ea13e8ae96e4eb6d6971b2cf6
Yuri Kuit <yuri.kuit+github@gmail.com>
2023-11-23 12:22:53 +0100

    cleanup 170

--- 170-install-cups.sh
+++ 170-install-cups.sh
@@ -2,19 +2,6 @@
 #set -e
 ##################################################################################################################
 # Author    : Erik Dubois
-# Website   : https://www.erikdubois.be
-# Website   : https://www.alci.online
-# Website   : https://www.ariser.eu
-# Website   : https://www.arcolinux.info
-# Website   : https://www.arcolinux.com
-# Website   : https://www.arcolinuxd.com
-# Website   : https://www.arcolinuxb.com
-# Website   : https://www.arcolinuxiso.com
-# Website   : https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
 ##################################################################################################################
 #tput setaf 0 = black
 #tput setaf 1 = red
@@ -39,38 +26,9 @@ echo "################################################################"
 tput sgr0
 echo
 
-func_install() {
-    if pacman -Qi $1 &> /dev/null; then
-        tput setaf 2
-        echo "###############################################################################"
-        echo "################## The package "$1" is already installed"
-        echo "###############################################################################"
-        echo
-        tput sgr0
-    else
-        tput setaf 3
-        echo "###############################################################################"
-        echo "##################  Installing package "  $1
-        echo "###############################################################################"
-        echo
-        tput sgr0
-        sudo pacman -S --noconfirm --needed $1
-    fi
-}
-
-
 installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 
-sudo pacman -S --noconfirm --needed cups
-sudo pacman -S --noconfirm --needed cups-pdf
-sudo pacman -S --noconfirm --needed ghostscript
-sudo pacman -S --noconfirm --needed gsfonts
-sudo pacman -S --noconfirm --needed gutenprint
-sudo pacman -S --noconfirm --needed gtk3-print-backends
-sudo pacman -S --noconfirm --needed libcups
-sudo pacman -S --noconfirm --needed system-config-printer
-sudo pacman -S --noconfirm --needed sane
-sudo pacman -S --noconfirm --needed simple-scan
+sudo pacman -S --noconfirm --needed sane simple-scan cups cups-pdf ghostscript gsfonts gutenprint gtk3-print-backends libcups system-config-printer
 
 sudo systemctl enable --now cups.service
 
